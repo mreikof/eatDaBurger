122a9e3f5e0a5acd6f7129eb41a2379bfc514eb0
mreikof <mike.reikofski@gmail.com>
2017-11-15 23:46:08 -0600

    update

---
+++ config/connection.js
@@ -0,0 +1,4 @@
+var mysql = require('mysql');
+
+// Create the MySQL connection object
+var connection;
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Bad Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('CheeseBurger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('SouthWestern Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Black Bean Veggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Buffalo Burger', true);
